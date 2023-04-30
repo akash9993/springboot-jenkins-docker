4b1baaf321c807e17bf092170a51dcb62c669f00
Akash <akash03@nagarro.com>
2023-04-30 10:21:32 +0530

    add new controller

--- src/main/java/com/nagarro/jenkinsdocker/controller/HomeController.java
+++ src/main/java/com/nagarro/jenkinsdocker/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.nagarro.jenkinsdocker.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +14,14 @@ public class HomeController {
 	@GetMapping("/status")
 	public String getStatus() {
 		return "Project running successfully | status code: 200 OK";
+	}
+
+	@GetMapping("/user")
+	public Map<String, String> getValues() {
+		Map<String, String> data = new HashMap<>();
+		data.put("Name", "Akash");
+		data.put("Technology", "Java");
+		data.put("Location", "Delhi");
+		return data;
 	}
 }
